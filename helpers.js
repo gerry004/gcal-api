8648e89e1477addb5504ca03e360ee69f9c47e71
Gerry Yang <gerry04y@gmail.com>
2024-01-21 22:29:19 +0000

    Refactor

--- helpers.js
+++ helpers.js
@@ -1,67 +1,74 @@
-const getCalendars = async (calendar, primary = true, calendarNames = []) => {
+const getCalendars = async (calendar, calendarNames = []) => {
   try {
-    const calendarList = await calendar.calendarList.list();
-    const calendars = calendarList.data.items;
+    const { data: { items: calendars } } = await calendar.calendarList.list();
 
     const filteredCalendars = calendars.filter(cal => {
-      if (primary && cal.primary) {
-        return true; // Include primary calendar
-      } else if (calendarNames.length > 0 && calendarNames.includes(cal.summary)) {
-        return true; // Include calendars in the calendarNames array
-      }
-      return false;
+      return (calendarNames.length > 0 && calendarNames.includes(cal.summary));
     });
 
     return filteredCalendars;
-  } 
-  catch (error) {
+  } catch (error) {
     console.error('Error fetching calendars:', error.message);
     return [];
   }
 };
 
-const getEvents = async (calendars, startDate, endDate) => {
+const getEvents = async (calendar, calendarId, startDate, endDate) => {
   try {
-    const allEvents = [];
-
-    for (const cal of calendars) {
-      const events = await cal.events.list({
-        calendarId: cal.id,
-        timeMin: startDate,
-        timeMax: endDate,
-        singleEvents: true,
-        orderBy: 'startTime',
-      });
-
-      if (events.data.items) {
-        allEvents.push(...events.data.items);
-      }
-    }
-
-    return allEvents;
-  } 
-  catch (error) {
+    const events = await calendar.events.list({
+      calendarId: calendarId,
+      timeMin: startDate,
+      timeMax: endDate,
+      singleEvents: true,
+      orderBy: 'startTime',
+    });
+    return events.data.items;
+  } catch (error) {
     console.error('Error fetching events:', error.message);
     return [];
   }
 };
 
-const sortEventsByColor = (events) => {
-  return // array of events sorted by color
+const sortEventsByColor = (events, defaultColorId) => {
+  const sortedEvents = {};
+  events.forEach(event => {
+    const color = event.colorId || defaultColorId;
+    sortedEvents[color] = sortedEvents[color] || [];
+    sortedEvents[color].push(event);
+  });
+  return sortedEvents;
+};
+
+const calculateTimeSpentByColor = (eventsByColor) => {
+  const timeSpentByColor = {};
+  Object.keys(eventsByColor).forEach(color => {
+    let timeSpentInMilliseconds = 0;
+    eventsByColor[color].forEach(event => {
+      const startDateTime = event.start.dateTime;
+      const endDateTime = event.end.dateTime;
+      const timeDifference = calculateTimeDifferenceInMilliseconds(startDateTime, endDateTime);
+      timeSpentInMilliseconds += timeDifference;
+    });
+    const { hours, minutes } = convertMillisecondsToHoursAndMinutes(timeSpentInMilliseconds);
+    timeSpentByColor[color] = { hours, minutes };
+  });
+  return timeSpentByColor;
 }
 
-const calculateTimeSpent = (events) => {
-  return // array of events with time spent
-}
-
-const convertMillisecondsToHours = (milliseconds) => {
-  return // hours
-}
+const convertMillisecondsToHoursAndMinutes = (milliseconds) => {
+  const totalMinutes = Math.floor(milliseconds / (1000 * 60));
+  const hours = Math.floor(totalMinutes / 60);
+  const minutes = totalMinutes % 60;
+  return { hours, minutes };
+};
 
 const calculateTimeDifferenceInMilliseconds = (startDateTime, endDateTime) => {
   const startDate = new Date(startDateTime);
   const endDate = new Date(endDateTime);
   const timeDifference = endDate - startDate;
+  if (isNaN(timeDifference)) {
+    return 0;
+  }
   return timeDifference;
 }
 
@@ -69,7 +76,5 @@ module.exports = {
   getCalendars,
   getEvents,
   sortEventsByColor,
-  calculateTimeSpent,
-  convertMillisecondsToHours,
-  calculateTimeDifferenceInMilliseconds,
+  calculateTimeSpentByColor
 };
--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const { google } = require('googleapis');
 const fs = require('fs').promises;
 const path = require('path');
 
-const {getCalendars, getEvents, sortEventsByColor, calculateTimeSpent, convertMillisecondsToHours, calculateTimeDifferenceInMilliseconds} = require('./helpers.js');
+const { getCalendars, getEvents, sortEventsByColor, calculateTimeSpentByColor } = require('./helpers.js');
+const { time } = require('console');
 
 const TOKEN_PATH = path.join(__dirname, 'token.json');
 const SCOPES = ['https://www.googleapis.com/auth/calendar'];
@@ -18,6 +19,27 @@ const oauth2Client = new google.auth.OAuth2(
   'http://localhost:3000/google/redirect'
 );
 const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
+
+const checkAuthenticationMiddleware = async (req, res, next) => {
+  try {
+    const tokens = JSON.parse(await fs.readFile(TOKEN_PATH));
+    oauth2Client.setCredentials(tokens);
+
+    if (oauth2Client.isTokenExpiring()) {
+      const { tokens: newTokens } = await oauth2Client.refreshToken(tokens.refresh_token);
+      oauth2Client.setCredentials(newTokens);
+      await fs.writeFile(TOKEN_PATH, JSON.stringify(newTokens));
+    }
+    req.oauth2Client = oauth2Client;
+    next();
+  }
+  catch (error) {
+    console.error('Error fetching tokens:', error.message);
+    res.status(500).send('Error fetching tokens');
+  }
+};
+
+app.use(checkAuthenticationMiddleware);
 
 // the page where the user signs in, redirects to /google/redirect
 app.get('/', async (req, res) => {
@@ -43,50 +65,38 @@ app.get('/google/redirect', async (req, res) => {
   }
 });
 
+const DEFAULT_COLOR_IDS = {
+  "gerry04y@gmail.com": 1,
+  "Gerry Piano": 2
+}
+
 app.get('/events', async (req, res) => {
   try {
-    const startDate = '2023-01-01T00:00:00Z'; // Replace with your start date
-    const endDate = '2023-01-07T23:59:59Z'; // Replace with your end date
-    const calendars = await getCalendars(calendar, true, ['Gerry Piano']);
-    const events = await getEvents(calendars, startDate, endDate);
+    const startDate = '2023-01-23T00:00:00Z'; // Replace with your start date
+    const endDate = '2023-01-23T23:59:59Z'; // Replace with your end date
+    const calendars = await getCalendars(calendar, ['gerry04y@gmail.com', 'Gerry Piano']);
+    calendars.forEach(async cal => {
+      console.log("foreach", cal.summary);
+      const events = await getEvents(calendar, cal.summary, startDate, endDate);
+      const eventsByColor = sortEventsByColor(events, DEFAULT_COLOR_IDS[cal.summary]);
+      const timeSpentByColor = calculateTimeSpentByColor(eventsByColor);
+      console.log(cal.summary);
+      console.log(timeSpentByColor);
+    });
 
-    console.log(calendars);
-    console.log(events);
-
-    res.json(eventsSummary);
-  } 
+    res.json(calendars);
+  }
   catch (error) {
     console.error('Error fetching events:', error.message);
     res.status(500).send('Error fetching events');
   }
 });
 
-const checkAuthenticationMiddleware = async (req, res, next) => {
-  try {
-    const tokens = JSON.parse(await fs.readFile(TOKEN_PATH));
-    oauth2Client.setCredentials(tokens);
-
-    if (oauth2Client.isTokenExpiring()) {
-      const { tokens: newTokens } = await oauth2Client.refreshToken(tokens.refresh_token);
-      oauth2Client.setCredentials(newTokens);
-      await fs.writeFile(TOKEN_PATH, JSON.stringify(newTokens));
-    }
-    req.oauth2Client = oauth2Client;
-    next();
-  }
-  catch (error) {
-    console.error('Error fetching tokens:', error.message);
-    res.status(500).send('Error fetching tokens');
-  }
-};
-
-app.use(checkAuthenticationMiddleware);
-
 app.listen(port, () => {
   try {
     console.log(`Starting Express Server...`);
     console.log(`Listening on Port:${port}`);
-  } 
+  }
   catch (error) {
     console.error('Error starting server:', error.message);
   }
