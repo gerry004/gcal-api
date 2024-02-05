3057fc2622bf7c37eac8b1d51d10fcb57a0f67f1
Gerry Yang <gerry04y@gmail.com>
2024-02-05 17:39:25 +0000

    Move Environment Variables

--- authentication.js
+++ authentication.js
@@ -1,14 +1,10 @@
 const fs = require('fs').promises;
 const { google } = require('googleapis');
 
-const CLIENT_ID = '95600525412-4qa3sr7ukqbssqpala8s1v1qsjrbjn5f.apps.googleusercontent.com';
-const CLIENT_SECRET = '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';
-const REDIRECT_URI = 'http://localhost:3000/google/redirect';
-
 const oauth2Client = new google.auth.OAuth2(
-  CLIENT_ID,
-  CLIENT_SECRET,
-  REDIRECT_URI
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
 );
 
 const isAuthenticated = async () => {
