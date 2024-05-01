a34f25e0daef0af8f97a32528ec10dacf307e087
Gerry Yang <gerry04y@gmail.com>
2024-05-01 17:56:34 +0100

    OAuth2Client

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ const fs = require('fs').promises;
 const path = require('path');
 const { oauth2Client, refreshToken, isAuthenticated } = require('./authentication.js');
 const { getCalendars, getEvents, sortEventsByColor, calculateTimeSpentByColor, concatenateArrayOfObjects, serialiseObject } = require('./helpers.js');
-
+const dotenv = require('dotenv');
+dotenv.config();
+const {OAuth2Client} = require('google-auth-library');
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname)));
 
-const port = 3000;
+const port = process.env.PORT;
 const TOKEN_PATH = path.join(__dirname, 'token.json');
 const SCOPES = ['https://www.googleapis.com/auth/calendar'];
 const calendar = google.calendar({ version: 'v3', auth: oauth2Client });
@@ -37,33 +39,99 @@ const COLOR_ID_LEGEND = {
   12: "Mandarin",
 }
 
-app.get('/', async (req, res) => {
-  if (await isAuthenticated()) {
-    await refreshToken(TOKEN_PATH);
-    res.redirect('/events');
-  }
-  else {
-    const url = oauth2Client.generateAuthUrl({
+// app.get('/', async (req, res) => {
+//   if (await isAuthenticated()) {
+//     await refreshToken(TOKEN_PATH);
+//     res.redirect('/events');
+//   }
+//   else {
+//     const url = oauth2Client.generateAuthUrl({
+//       access_type: 'offline',
+//       scope: SCOPES
+//     });
+//     res.redirect(url);
+//   }
+// });
+
+app.post('/request', async function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", 'http://localhost:8080');
+  res.header("Access-Control-Allow-Credentials", 'true');
+  res.header("Referrer-Policy","no-referrer-when-downgrade");
+
+  const redirectURL = 'http://localhost:3000/google/redirect';
+
+  const oAuth2Client = new OAuth2Client(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+      redirectURL
+    );
+
+    const authorizeUrl = oAuth2Client.generateAuthUrl({
       access_type: 'offline',
-      scope: SCOPES
+      scope: 'https://www.googleapis.com/auth/userinfo.profile  openid https://www.googleapis.com/auth/calendar',
+      prompt: 'consent'
     });
-    res.redirect(url);
-  }
+
+    res.json({url:authorizeUrl})
+
 });
 
-app.get('/google/redirect', async (req, res) => {
-  try {
+async function getUserData(access_token) {
+
+  const response = await fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`);
+  
+  //console.log('response',response);
+  const data = await response.json();
+  console.log('data',data);
+}
+
+
+
+/* GET home page. */
+app.get('/google/redirect', async function(req, res, next) {
+
     const code = req.query.code;
-    const { tokens } = await oauth2Client.getToken(code);
-    oauth2Client.setCredentials(tokens);
-    await fs.writeFile(TOKEN_PATH, JSON.stringify(tokens));
-    res.redirect('/events');
-  }
-  catch (error) {
-    console.error('Error authenticating:', error.message);
-    res.status(500).send('Error authenticating');
-  }
+
+    console.log(code);
+    try {
+        const redirectURL = "http://127.0.0.1:3000/google/redirect"
+        const oAuth2Client = new OAuth2Client(
+            process.env.CLIENT_ID,
+            process.env.CLIENT_SECRET,
+            redirectURL
+          );
+        const r =  await oAuth2Client.getToken(code);
+        await oAuth2Client.setCredentials(r.tokens);
+        console.info('Tokens acquired.');
+        const user = oAuth2Client.credentials;
+        console.log('credentials',user);
+        await getUserData(oAuth2Client.credentials.access_token);
+
+      } catch (err) {
+        console.log('Error logging in with OAuth2 user', err);
+    }
+
+
+    res.redirect(303, 'http://localhost:8080/');
+  
+
+
 });
+
+// app.get('/google/redirect', async (req, res) => {
+//   try {
+//     console.log('Redirecting...');
+//     const code = req.query.code;
+//     const { tokens } = await oauth2Client.getToken(code);
+//     oauth2Client.setCredentials(tokens);
+//     await fs.writeFile(TOKEN_PATH, JSON.stringify(tokens));
+//     res.redirect('/events');
+//   }
+//   catch (error) {
+//     console.error('Error authenticating:', error.message);
+//     res.status(500).send('Error authenticating');
+//   }
+// });
 
 app.get('/events', async (req, res) => {
   if (await isAuthenticated()) {
