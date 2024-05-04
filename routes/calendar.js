a3ba174c26cd910f847def775f3c3621aef2bf37
Gerry Yang <gerry04y@gmail.com>
2024-05-04 18:36:26 +0100

    Calendars

--- routes/calendar.js
+++ routes/calendar.js
@@ -7,7 +7,7 @@ const secretKey = process.env.SECRET_KEY;
 
 const calendar = google.calendar({ version: 'v3', auth: authClient });
 
-function jwtAuthMiddleware(req, res, next) {
+const isAuthenticated = (req, res, next) => {
   const jwtToken = req.cookies.jwtToken;
   if (!jwtToken) {
     return res.status(401).send('Unauthorized');
@@ -22,7 +22,7 @@ function jwtAuthMiddleware(req, res, next) {
   });
 }
 
-router.get('/colors', jwtAuthMiddleware, async (req, res) => {
+router.get('/colors', isAuthenticated, async (req, res) => {
   try {
     const response = await calendar.colors.get();
     const eventColors = response.data.event;
@@ -32,7 +32,7 @@ router.get('/colors', jwtAuthMiddleware, async (req, res) => {
   }
 });
 
-router.get("/events", jwtAuthMiddleware, async (req, res) => {
+router.get("/events", isAuthenticated, async (req, res) => {
   try {
     const startDate = req.query.startDate + 'T00:00:00Z';
     const endDate = req.query.endDate + 'T00:00:00Z';
@@ -64,7 +64,7 @@ router.get("/events", jwtAuthMiddleware, async (req, res) => {
   }
 });
 
-router.get('/calendars', jwtAuthMiddleware, async (req, res) => {
+router.get('/calendars', isAuthenticated, async (req, res) => {
   try {
     const response = await calendar.calendarList.list();
     res.json({ calendars: response.data.items });
@@ -74,25 +74,3 @@ router.get('/calendars', jwtAuthMiddleware, async (req, res) => {
 });
 
 module.exports = router;
-
-// app.post('/events', async (req, res) => {
-//   try {
-//     const { startDate, endDate } = req.body;
-//     const calendars = await getCalendars(calendar, ['gerry04y@gmail.com', 'Gerry Piano']);
-
-//     const timeSpentByColors = await Promise.all(calendars.map(async cal => {
-//       const events = await getEvents(calendar, cal.id, startDate, endDate);
-//       const eventsByColor = sortEventsByColor(events, DEFAULT_COLOR_IDS[cal.summary]);
-//       const timeSpentByColor = calculateTimeSpentByColor(eventsByColor);
-//       return timeSpentByColor;
-//     }));
-
-//     const timeSpentByColor = concatenateArrayOfObjects(timeSpentByColors);
-//     const timeSpentByColorWithColorNames = serialiseObject(timeSpentByColor, COLOR_ID_LEGEND);
-//     res.json(timeSpentByColorWithColorNames);
-//   }
-//   catch (error) {
-//     console.error('Error fetching events:', error.message);
-//     res.status(500).send('Error fetching events');
-//   }
-// });
