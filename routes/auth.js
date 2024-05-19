56f3cb6b7879d9fd3576e42f56677543f7e36c13
Gerry Yang <gerry04y@gmail.com>
2024-05-19 21:03:09 +0100

    Refreshing Token

--- routes/auth.js
+++ routes/auth.js
@@ -18,9 +18,13 @@ router.get('/google/redirect', async function (req, res, next) {
 
   try {
     const { tokens } = await authClient.getToken(code);
-    const jwtToken = jwt.sign(tokens, process.env.SECRET_KEY, { expiresIn: '1h' });
+
+    const jwtToken = jwt.sign(tokens, process.env.SECRET_KEY, { expiresIn: '1d' });
+    const refreshToken = jwt.sign({ refreshToken: tokens.refresh_token }, process.env.SECRET_KEY, { expiresIn: '14d' })
 
     res.cookie('jwtToken', jwtToken, { httpOnly: true, secure: false });
+    res.cookie('refreshToken', refreshToken, { httpOnly: true, secure: false });
+
     res.redirect('http://localhost:8080/dashboard');
 
   } catch (err) {
--- routes/calendar.js
+++ routes/calendar.js
@@ -7,19 +7,55 @@ const secretKey = process.env.SECRET_KEY;
 
 const calendar = google.calendar({ version: 'v3', auth: authClient });
 
-const isAuthenticated = (req, res, next) => {
+const isExpiring = (exp) => {
+  const currentTime = Math.floor(Date.now() / 1000);
+  const timeToExpire = exp - currentTime;
+  return timeToExpire < 7200; // 2 hours before it expires
+}
+
+const refreshTokens = async (refreshToken) => {
+  try {
+    const newTokens = await authClient.refreshToken(refreshToken);
+    return newTokens.tokens;
+  } catch (err) {
+    console.error('Error refreshing token:', err);
+    throw err;
+  }
+}
+
+const isAuthenticated = async (req, res, next) => {
   const jwtToken = req.cookies.jwtToken;
+  const refreshToken = req.cookies.refreshToken;
+
   if (!jwtToken) {
     return res.status(401).send('Unauthorized');
   }
 
-  jwt.verify(jwtToken, secretKey, (err, decoded) => {
-    if (err) {
-      return res.status(401).send('Unauthorized');
+  try {
+    const decoded = jwt.verify(jwtToken, secretKey);
+
+    if (isExpiring(decoded.exp)) {
+      const decodedRefreshToken = jwt.verify(refreshToken, secretKey);
+
+      if (decodedRefreshToken) {
+        const newTokens = await refreshTokens(decodedRefreshToken.refreshToken);
+        const newJwtToken = jwt.sign(newTokens, secretKey, { expiresIn: '1d' });
+        const newDecoded = jwt.verify(newJwtToken, secretKey);
+
+        authClient.setCredentials(newDecoded);
+        res.cookie('jwtToken', newJwtToken, { httpOnly: true, secure: false });
+      } else {
+        return res.status(401).send('Unauthorized');
+      }
+    } else {
+      authClient.setCredentials(decoded);
     }
-    authClient.setCredentials(decoded);
+
     next();
-  });
+  } catch (error) {
+    console.error('Error authenticating:', error);
+    return res.status(401).send('Unauthorized');
+  }
 }
 
 router.get('/colors', isAuthenticated, async (req, res) => {
@@ -48,7 +84,7 @@ router.get("/events", isAuthenticated, async (req, res) => {
           singleEvents: true,
           orderBy: 'startTime',
         });
-        
+
         return response.data.items.map((event) => ({
           ...event,
           colorId: event.colorId || cal.defaultEventColor,
@@ -61,7 +97,7 @@ router.get("/events", isAuthenticated, async (req, res) => {
 
     const results = await Promise.all(eventsPromises);
     const events = results.flat();
-    
+
     res.json({ events });
   } catch (error) {
     console.error('Error processing events:', error.message);
